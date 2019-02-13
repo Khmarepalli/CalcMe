f9aec5f466f1bb0922af456e19ad94eb1dd173ed
Krishna Marepalli <khmarepalli@gmail.com>
2019-02-12 19:20:13 -0600

    Need to finish implementing decimal numbers to finish app.

--- CalcMe/ViewController.swift
+++ CalcMe/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         if numberOneBool == true
         {
             numberOne = 0
+            Label.text = "\(numberOne)"
         }
         else if numberTwoBool == true
         {
@@ -26,10 +27,12 @@ class ViewController: UIViewController {
                 numberOne = 0
                 numberTwoBool = false
                 numberOneBool = true
+                Label.text = "\(numberOne)"
             }
             else
             {
                 numberTwo = 0
+                Label.text = "\(numberTwo)"
             }
         }
     }
@@ -38,95 +41,170 @@ class ViewController: UIViewController {
         {
             var stringNumber = "\(numberOne)"
             stringNumber.remove(at: stringNumber.index(before: stringNumber.endIndex))
-            
+            Label.text = stringNumber
         }
         else if numberTwoBool == true
         {
             var stringNumber = "\(numberTwo)"
             stringNumber.remove(at: stringNumber.index(before: stringNumber.endIndex))
-            
+            Label.text = stringNumber
         }
+        
 
     }
     @IBAction func BtnNo9(_ sender: Any) {
-        if numberOneBool == true
-        {
-            if numberOne == 0
-            {
-                numberOne = 9
-            }
-            else
-            {
-                var stringNumber = "\(numberOne)"
-                stringNumber += "9"
-            }
-        }
-        else if numberTwoBool == true
-        {
-            if numberOne == 0
-            {
-                numberOne = 9
-            }
-            else
-            {
-                var stringNumber = "\(numberTwo)"
-                stringNumber += "9"
-            }
-
-        }
-
+        setNumber(number: 9)
     }
     @IBAction func BtnNo8(_ sender: Any) {
-        
+        setNumber(number: 8)
     }
     @IBAction func BtnNo7(_ sender: Any) {
-        
+        setNumber(number: 7)
     }
     @IBAction func BtnNo6(_ sender: Any) {
-        
+        setNumber(number: 6)
     }
     @IBAction func BtnNo5(_ sender: Any) {
-        
+        setNumber(number: 5)
     }
     @IBAction func BtnNo4(_ sender: Any) {
-        
+        setNumber(number: 4)
     }
     @IBAction func BtnNo3(_ sender: Any) {
-        
+        setNumber(number: 3)
     }
     @IBAction func BtnNo2(_ sender: Any) {
-        
+        setNumber(number: 2)
     }
     @IBAction func BtnNo1(_ sender: Any) {
-        
+        setNumber(number: 1)
     }
     @IBAction func BtnNo0(_ sender: Any) {
-        
+        setNumber(number: 0)
     }
     @IBAction func BtnNoDecimal(_ sender: Any) {
         
     }
     @IBAction func BtnDivide(_ sender: Any) {
-        
+        if numberTwoBool == true
+        {
+            numberOne /= numberTwo
+            numberTwoBool = false
+            numberOneBool = true
+            Label.text = "0"
+        }
+        else if numberOneBool == true
+        {
+            numberTwoBool = true
+            numberOneBool = false
+            Label.text = "0"
+        }
+
     }
     @IBAction func BtnMultiplication(_ sender: Any) {
-        
+        if numberTwoBool == true
+        {
+            numberOne *= numberTwo
+            numberTwoBool = false
+            numberOneBool = true
+            Label.text = "0"
+        }
+        else if numberOneBool == true
+        {
+            numberTwoBool = true
+            numberOneBool = false
+            Label.text = "0"
+        }
+
     }
     @IBAction func BtnSubtract(_ sender: Any) {
-        
+        if numberTwoBool == true
+        {
+            numberOne -= numberTwo
+            numberTwoBool = false
+            numberOneBool = true
+            Label.text = "0"
+        }
+        else if numberOneBool == true
+        {
+            numberTwoBool = true
+            numberOneBool = false
+            Label.text = "0"
+        }
+
     }
     @IBAction func BtnAddition(_ sender: Any) {
-        
+        if numberTwoBool == true
+        {
+            numberOne += numberTwo
+            numberTwoBool = false
+            numberOneBool = true
+            Label.text = "0"
+        }
+        else if numberOneBool == true
+        {
+            numberTwoBool = true
+            numberOneBool = false
+            Label.text = "0"
+        }
     }
     @IBAction func BtnEquals(_ sender: Any) {
-        
+        numberOne = numberOne + numberTwo
+        numberTwo = 0
+        Label.text = numberOne.withCommas()
+        numberOneBool = true
+        numberTwoBool = false
+    }
+    
+    func setNumber(number: Int)
+    {
+        if numberOneBool == true
+        {
+            if numberOne == 0
+            {
+                numberOne = number
+                Label.text = numberOne.withCommas()
+            }
+            else
+            {
+                var stringNumber = "\(numberOne)"
+                stringNumber = stringNumber + String(number)
+                numberOne = Int(stringNumber)!
+                Label.text = numberOne.withCommas()
+            }
+        }
+        else if numberTwoBool == true
+        {
+            if numberTwo == 0
+            {
+                numberTwo = number
+                Label.text = numberTwo.withCommas()
+            }
+            else
+            {
+                var stringNumber = "\(numberTwo)"
+                stringNumber += String(number)
+                numberTwo = Int(stringNumber)!
+                Label.text = numberTwo.withCommas()
+            }
+        }
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Label.text = "0"
     }
 
 
+}
+
+extension Int {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        return numberFormatter.string(from: NSNumber(value:self))!
+    }
 }
 
